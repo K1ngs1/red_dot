7e83eacceb8988086883446dc24be67b5a6d42fb
K1ngs1 <76851907+K1ngs1@users.noreply.github.com>
2022-04-28 17:24:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+nosex = 0;
+nosey = 0;
+
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet ready');
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, nosex, nosey, 30, 30);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        nosex =  results[0].pose.nose.x - 10;
+        nosey =  results[0].pose.nose.y - 10;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}       
+
+function takeSnapshot(){
+    save('red-nose.jpg');
+} 
+
+function save(){
+    
+
+}
